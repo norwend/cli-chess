b771ebb036339b50efcc4c1f6baa9ad7ce9e0363
Stepan Ilyin <norwend@tuta.io>
2021-03-28 21:43:24 +0300

    finished drawing all figures

--- board.hpp
+++ board.hpp
@@ -3,15 +3,23 @@
 
 class Board {
 public:
-	void move_figure();
+	void move_figure(const int&, const int&, const int&, const int&);
 	void print();
 	Board (std::array<std::array<int, 8>, 8> b) : board_(b) {}
 private:
 	std::array<std::array<int,8>, 8> board_;
 };
 
-void move_figure(int& pos1, int& pos2) {
-	
+void Board::move_figure(const int& pos1, const int& pos2, const int& pos1_n, const int& pos2_n) {
+	switch (board_[pos1][pos2]) {
+		case 1:
+			std::cout << pos1 << pos2 << pos1_n << pos2_n;
+		if (pos2 == pos2_n && pos1_n - pos1 <= 2 && pos1 == 6) {
+			board_[pos1][pos2] = 0;
+			board_[pos1_n][pos2_n] = 1;
+		}
+		break;
+	}
 }
 
 void Board::print() { // Ужасно!
@@ -24,8 +32,38 @@ void Board::print() { // Ужасно!
 				case 1:
 					std::cout << "♟︎ ";
 					break;
+				case 2:
+					std::cout << "♝ ";
+					break;
+				case 3:
+					std::cout << "♞ ";
+					break;
+				case 4:
+					std::cout << "♜ ";
+					break;
+				case 5:
+					std::cout << "♛ ";
+					break;
+				case 6:
+					std::cout << "♚ ";
+					break;
 				case -1:
 					std::cout << "♙ ";
+					break;
+				case -2:
+					std::cout << "♗ ";
+					break;
+				case -3:
+					std::cout << "♘ ";
+					break;
+				case -4:
+					std::cout << "♖ ";
+					break;
+				case -5:
+					std::cout << "♕ ";
+					break;
+				case -6:
+					std::cout << "♔ ";
 					break;
 				default:
 					break;
--- main.cpp
+++ main.cpp
@@ -12,5 +12,13 @@ int main() {
 		{4, 3, 2, 5, 6, 2, 3, 4}
 	}});
 	main_board.print();
+	short p1,p2,p3,p4 = 0;
+	while (true) {
+		std::cout << "Ваш ход? (пока только пешки)\n> ";
+		std::cin >> p1 >> p2 >> p3 >> p4;
+		main_board.move_figure(p1, p2, p3, p4);
+		main_board.print();
+		p1,p2,p3,p4 = 0;
+	}
 	return 0;
 }
